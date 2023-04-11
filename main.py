7e0ca0c5301dbb43939da2a5459eb41accc030dc
Ashutosh Chandra <97390449+Ashutosh-Chandra5@users.noreply.github.com>
2023-04-11 10:39:14 +0530

    Added main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# importing the required modules
+from tkinter import *                   # importing all the modules and classes from tkinter
+from tkinter import ttk as ttk          # importing the ttk module from tkinter
+from tkinter import messagebox as mb    # importing the messagebox module from tkinter
+import datetime                         # importing the datetime module
+import sqlite3                          # importing the sqlite3 module
+from tkcalendar import DateEntry        # importing the DateEntry class from the tkcalendar module
+
